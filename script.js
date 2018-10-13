f9af6bda5191f33fc058f5f12f9a83863daecd64
Derek <good1derek@gmail.com>
2018-10-13 00:04:42 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAxuVDmAbBG_WQoWbe5Qace2lJ9_KH5Buw",
+    authDomain: "contact-3f03f.firebaseapp.com",
+    databaseURL: "https://contact-3f03f.firebaseio.com",
+    projectId: "contact-3f03f",
+    storageBucket: "contact-3f03f.appspot.com",
+    messagingSenderId: "847446540377"
+  };
+  firebase.initializeApp(config);
