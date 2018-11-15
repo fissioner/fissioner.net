83b5eecd6d67b3c210dfe191473995404ed91ee9
Derek <26776606+fissioner@users.noreply.github.com>
2018-11-14 20:39:36 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,42 +7,5 @@ const exphbs = require('express-handlebars');
 
 
 
-app.post('/sent', (req, res) => {
-  
-  const sender = req.body.email;
-  const formContent = `
-  <h3>New Contact</h3>
-  <ul>
-    <li>Name: ${req.body.firstName} ${req.body.lastName}</li>
-    <li>Email: ${req.body.email}</li>
-    <li>Phone: ${!req.body.phone?'none provided':req.body.phone}</li>
-    <li>Company: ${!req.body.company?'none provided': req.body.company}</li>
-  </ul>
-  <h2>Message</h4>
-  <p>${req.body.message}</p>
-  `;
-
-  const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: process.env.SENDGRID_RECIPIENT,
-  from: sender,
-  subject: 'Fissioner Contact',
-  text: formContent,
-  html: formContent,
-};
-sgMail.send(msg).then(() => {
-  res.status(200);
-  console.log('email sent');
-  return;
-})
-.catch(error => {
-  return console.error(error.toString());
-});
-
-console.log(req.body)
-res.render('index', {sent: true, msg: 'Your message has been sent.'})
-})
-
 const port = process.env.PORT || process.env.PORT_NUM || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
